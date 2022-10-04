32ef607505e5095ebb74e401164ec4a10c5ed54f
Pragmatic <cryptopragmatic@protonmail.com>
2022-10-04 02:30:58 +0200

    Updates

--- app.ts
+++ app.ts
@@ -1076,8 +1076,8 @@ const AINTADDRESS = "3PBmmxKhFcDhb8PrDdCdvw2iGMPnp7VuwPy"
 
 var activeScreen = "home";
 var earningsScript = "https://aint.kriptokuna.com";
-var mobileNodeUrl = "http://localhost:5001";
-// var mobileNodeUrl = "https://mobile.anote.digital";
+// var mobileNodeUrl = "http://localhost:5001";
+var mobileNodeUrl = "https://mobile.anote.digital";
 var t;
 
 const wallet = new Wallet();
